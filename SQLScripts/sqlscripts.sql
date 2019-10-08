ce08920ec1971685569003373d4704ece91fba50
sbijukchhe <skbijukchhe@gmail.com>
2019-10-08 14:16:15 -0400

    Database relationship ManyToMany

---
+++ SQLScripts/sqlscripts.sql
@@ -0,0 +1,8 @@
+create database MyDatabase;
+
+select * from course;
+select * from student;
+select * from student_course;
+
+
+-- Each Student takes Many Courses. And Each Course has Many Students.
